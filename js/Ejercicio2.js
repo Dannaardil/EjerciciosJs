0eba523973aa10da6115f63854421042910a6c4c
Dannaardil <156458170+Dannaardil@users.noreply.github.com>
2024-04-15 22:12:25 -0500

    feat: :construction: se esta terminando el ejercicio 4

---
+++ js/Ejercicio2.js
@@ -0,0 +1,18 @@
+import prompt from "prompt-sync"
+let input = prompt();
+
+let cal_impuestos = function(){
+    let edad = input("ingrese su edad: ")
+    let ingresos = input( "ingrese sus ingresos: ")
+    
+    if (edad>= 18 && ingresos>= 1000){
+       let  impuestos = (ingresos/100)*40;
+       console.log("impuestos a pagar: " + impuestos)
+    }
+    else {
+        console.log ("Usted no aplica para pagar impuestos ")
+    }
+    
+
+}
+cal_impuestos()
