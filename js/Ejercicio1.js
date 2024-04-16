6f6239ae98a762fe34fce319990d1796ec098bb3
Dannaardil <156458170+Dannaardil@users.noreply.github.com>
2024-04-15 22:12:25 -0500

    feat: :construction: se esta terminando el ejercicio 4

--- js/Ejercicio1.js
+++ js/Ejercicio1.js
@@ -4,16 +4,16 @@ let contra = function(){
     let contraseña = input("Ingrese su contraseña: ");
     let con_1 = "2Fj(jjbFsuj"; 
     let con_2 = "eoZiugBf&g9";
-
-
+    
+    
     if (contraseña === con_1 || contraseña=== con_2){
         console.log("True")
-
+    
     }
     else{
         console.log("False")
     }
-
+    
     }
-
+    
     contra();
--- js/Ejercicio2.js
+++ js/Ejercicio2.js
@@ -4,7 +4,7 @@ let input = prompt();
 let cal_impuestos = function(){
     let edad = input("ingrese su edad: ")
     let ingresos = input( "ingrese sus ingresos: ")
-
+    
     if (edad>= 18 && ingresos>= 1000){
        let  impuestos = (ingresos/100)*40;
        console.log("impuestos a pagar: " + impuestos)
@@ -12,7 +12,7 @@ let cal_impuestos = function(){
     else {
         console.log ("Usted no aplica para pagar impuestos ")
     }
-
+    
 
 }
 cal_impuestos()
