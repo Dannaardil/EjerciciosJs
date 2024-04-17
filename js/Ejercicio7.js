8c60b5460ec16bcdf5031001ab4e49cd1eadf252
Dannaardil <156458170+Dannaardil@users.noreply.github.com>
2024-04-16 22:31:11 -0500

    feat: :construction: se comenzo el ejercicio 7 y 8

--- js/Ejercicio7.js
+++ js/Ejercicio7.js
@@ -1,2 +1,21 @@
 import prompt from "prompt-sync"
 let input = prompt();
+
+let contarRango = function(){
+    let numero = input("ingrese el primer numero: ")
+    let numero2 = input("ingrese el segundo numero: ")
+   for (let i in numero){
+    let numero_s = 0
+    if(numero == numero2){
+        console.log("0")
+    }
+    else if (numero != numero2){
+        numero_s + (numero2-numero)
+        console.log(numero_s)
+
+    }
+ }
+}
+
+
+contarRango()
