98d50ba82856c3e8c19f4a20f7965ba6e2bfb159
Dannaardil <156458170+Dannaardil@users.noreply.github.com>
2024-04-18 00:50:13 -0500

    feat: :construction: se realizo el ejercicio numero 8

--- js/Ejercicio8.js
+++ js/Ejercicio8.js
@@ -1,2 +1,15 @@
 import prompt from "prompt-sync"
 let input = prompt();
+
+function sumarRango(numInicial, numFinal) {
+    let suma = 0;
+    for (let i = numInicial; i <= numFinal; i++) {
+        suma += i;
+    }
+    return suma;
+}
+
+
+console.log(sumarRango(0, 10));   // Salida: 55
+console.log(sumarRango(12, 14));  // Salida: 39
+console.log(sumarRango(5, 5));    // Salida: 5
