19e63cd36ada6025a18dff5f19385142a1ad233b
Dannaardil <156458170+Dannaardil@users.noreply.github.com>
2024-04-18 01:38:52 -0500

    feat: :construction: se eesta realizando el ejercicio 17

--- js/ejercicio17.js
+++ js/ejercicio17.js
@@ -1,3 +1,32 @@
 import prompt from "prompt-sync"
 let input = prompt();
 
+function capitalizar(string){
+    let resultado = ""
+   for (let i in string){
+       if (i==0 ){
+           resultado+= string[0].toUpperCase()
+          
+       }
+       else if(string[i]== " "){
+        i++
+        
+        resultado+= " " +string[i].toUpperCase()
+      
+         
+      
+    
+
+       }
+       else{
+           
+           resultado+= string[i]
+       }
+   }
+   return (resultado)
+}
+console.log(capitalizar("hola mundo")) // "Hola Mundo"
+console.log(capitalizar("make it real")) // "Make It Real"
+console.log(capitalizar("")) // ""
+
+
