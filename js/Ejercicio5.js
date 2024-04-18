1636a9f32ad102106ac65d5edc40eca414949df7
Dannaardil <156458170+Dannaardil@users.noreply.github.com>
2024-04-18 00:49:39 -0500

    feat: :construction: se realizo el ejercicio numero 5

--- js/Ejercicio5.js
+++ js/Ejercicio5.js
@@ -2,3 +2,16 @@ import prompt from "prompt-sync";
 
 let input = prompt();
 
+
+ let likes = function(){
+        let number = input("ingrese el numero de likes: ")
+        if (number < 1000) {
+            return console.log(number.toString());
+        } else if (number < 1000000) {
+            return console.log(Math.floor(number / 1000) + 'K');
+        } else {
+            return console.log(Math.floor(number / 1000000) + 'M');
+        }
+    }
+
+likes()
